9e45f28ae56876fe8428a0dc03a4151baed39851
danydim03 <151035632+danydim03@users.noreply.github.com>
2025-12-26 16:36:45 +0100

    FS persistency added to the system, I managed to set up the CSV manager which has the responsibility to call some important operations. moreover, all the needed FilesystemDAO have been implemented and fully work during the test .

---
+++ src/main/java/org/example/dao_manager/DAOFactoryFS.java
@@ -0,0 +1,58 @@
+package org.example.dao_manager;
+
+import org.example.model.food.DAO.FoodDAOFS;
+import org.example.model.food.DAO.FoodDAOInterface;
+import org.example.model.ordine.DAO.OrdineDAOFS;
+import org.example.model.ordine.DAO.OrdineDAOInterface;
+import org.example.model.role.Amministratore.DAO.AmministratoreDAOFS;
+import org.example.model.role.Amministratore.DAO.AmministratoreDAOInterface;
+import org.example.model.role.Cliente.DAO.ClienteDAOFS;
+import org.example.model.role.Cliente.DAO.ClienteDAOInterface;
+import org.example.model.role.Kebabbaro.DAO.KebabbaroDAOFS;
+import org.example.model.role.Kebabbaro.DAO.KebabbaroDAOInterface;
+import org.example.model.user.DAO.UserDAOFS;
+import org.example.model.user.DAO.UserDAOInterface;
+import org.example.model.voucher.DAO.VoucherDAOFS;
+import org.example.model.voucher.DAO.VoucherDAOInterface;
+
+/**
+ * Factory per la creazione di DAO basati su File System (CSV).
+ * Implementa il pattern Abstract Factory per la persistenza su file CSV.
+ */
+public class DAOFactoryFS extends DAOFactoryAbstract {
+
+    @Override
+    public KebabbaroDAOInterface getKebabbaroDAO() {
+        return KebabbaroDAOFS.getInstance();
+    }
+
+    @Override
+    public AmministratoreDAOInterface getAmministratoreDAO() {
+        return AmministratoreDAOFS.getInstance();
+    }
+
+    @Override
+    public ClienteDAOInterface getClienteDAO() {
+        return ClienteDAOFS.getInstance();
+    }
+
+    @Override
+    public OrdineDAOInterface getOrdineDAO() {
+        return OrdineDAOFS.getInstance();
+    }
+
+    @Override
+    public VoucherDAOInterface getVoucherDAO() {
+        return VoucherDAOFS.getInstance();
+    }
+
+    @Override
+    public UserDAOInterface getUserDAO() {
+        return UserDAOFS.getInstance();
+    }
+
+    @Override
+    public FoodDAOInterface getFoodDAO() {
+        return FoodDAOFS.getInstance();
+    }
+}
